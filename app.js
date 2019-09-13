faf2df8ec16f8f8ec6bfc605e39cf1c41d85041b
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-09-13 16:00:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+// Gestionar las sessiones de los usuarios con mongo
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
+const indexRouter = require('./routes/index')
+
+const app = express()
+
+// npm install mongoose
+const mongoose = require('mongoose')
+// --
+mongoose.connect('mongodb://localhost/footballApp', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+})
+
+// Se encarga de gestionar si hay cookie. Si hay cookie la identifica con el id
+// Si no hay crea una nueva
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
