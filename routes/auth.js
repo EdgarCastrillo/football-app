c3b2f2f8ea4fa3a4566dfbc3f0bb690ce8cad588
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-09-14 11:30:33 +0200

    signup and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,58 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
-/* GET home page. */
+const User = require('../models/Users')
+
 router.get('/signup', (req, res, next) => {
   res.render('signup', { title: 'Auth-app' })
 })
 
+router.post('/signup', async (req, res, next) => {
+  const { username, password } = req.body
+  try {
+    const salt = bcrypt.genSaltSync(saltRounds)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+    const user = await User.findOne({ username })
+    if (user) {
+      res.redirect('/auth/signup')
+    } else {
+      const newUser = User.create({
+        username: username,
+        password: hashedPassword
+      })
+      req.session.currentUser = newUser
+      res.redirect('/')
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.get('/login', (req, res, next) => {
-  res.render('login', { title: 'Auth-app' })
+  res.render('login')
+})
+
+router.post('/login', async (req, res, next) => {
+  const { username, password } = req.body
+  console.log(req.body)
+  try {
+    const user = await User.findOne({ username })
+    if (!user) {
+      return next()
+    }
+    // Login user
+    if (bcrypt.compareSync(password, user.password)) {
+      req.session.currentUser = user
+      console.log(req.session.currentUser)
+      res.redirect('/')
+    } else {
+      res.redirect('/auth/login')
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
